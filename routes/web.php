979c93d773b413878f786ae2669fe0d44e644ac3
Minh Nguyen <minhng@haposoft.com>
2024-12-19 14:12:36 +0700

    Simply display all current jobs on the screen

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::resource('jobs', JobController::class)
     ->only(['index', 'show']);
