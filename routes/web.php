0bb640e773efda8eec8c228440c77bf25d381a7f
Minh Nguyen <minhng@haposoft.com>
2024-12-19 11:16:10 +0700

    Add route from the JobController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('jobs.index');
 });
+
+Route::resource('jobs', JobController::class)
+    ->only(['index', 'show']);
