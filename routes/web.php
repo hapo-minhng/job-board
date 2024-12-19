bc87f6f0ec3b1d2db12506e498f10215ec8797ad
Minh Nguyen <minhng@haposoft.com>
2024-12-19 13:48:53 +0700

    Apply Tailwind

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
 Route::get('/', function () {
-    return redirect()->route('jobs.index');
+    return view('welcome');
 });
 
 Route::resource('jobs', JobController::class)
