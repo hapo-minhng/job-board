680570565cc5086fe88d7454836c564cb4974a34
Minh Nguyen <minhng@haposoft.com>
2024-12-19 13:08:00 +0700

    Add index view for job

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Http\Request;
+use App\Models\Job;
 
 class JobController extends Controller
 {
