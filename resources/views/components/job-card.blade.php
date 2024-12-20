f55147c171e94867ae17a647e06017898879b32c
Minh Nguyen <minhng@haposoft.com>
2024-12-20 09:53:00 +0700

    Filter jobs by title or description

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -20,10 +20,6 @@
             </x-tag>
         </div>
     </div>
-
-    <p class="text-sm text-slate-500">
-        {!! nl2br(e($job->description)) !!}
-    </p>
-
+    
     {{ $slot }}
 </x-card>
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,25 +2,29 @@
     <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index')]" />
 
     <x-card class="mb-4 text-sm">
-        <div class="mb-4 grid grid-cols-2 gap-4">
-            <div>
-                <div class="mb-1 font-semibold">
-                    Jobs
-                </div>
-                <x-text-input name="search" value="" placeholder="Search for any text" />
-            </div>
-
-            <div>
-                <div class="mb-1 font-semibold">
-                    Salary
+        <form action="{{ route('jobs.index') }}" method="GET">
+            <div class="mb-4 grid grid-cols-2 gap-4">
+                <div>
+                    <div class="mb-1 font-semibold">
+                        Jobs
+                    </div>
+                    <x-text-input name="search" value="" placeholder="Search for any text" />
                 </div>
 
-                <div class="flex space-x-2">
-                    <x-text-input name="min_salary" value="" placeholder="From" />
-                    <x-text-input name="max_salary" value="" placeholder="To" />
+                <div>
+                    <div class="mb-1 font-semibold">
+                        Salary
+                    </div>
+
+                    <div class="flex space-x-2">
+                        <x-text-input name="min_salary" value="" placeholder="From" />
+                        <x-text-input name="max_salary" value="" placeholder="To" />
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <x-button class="w-full">Filter</x-button>
+        </form>
     </x-card>
 
     @foreach ($jobs as $job)
