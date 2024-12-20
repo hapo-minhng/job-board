16d08b52c7f32543bf62bb2e754d6bc96e08d5ae
Minh Nguyen <minhng@haposoft.com>
2024-12-20 10:30:43 +0700

    Filter jobs by salary

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -8,7 +8,7 @@
                     <div class="mb-1 font-semibold">
                         Jobs
                     </div>
-                    <x-text-input name="search" value="" placeholder="Search for any text" />
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" />
                 </div>
 
                 <div>
@@ -17,8 +17,8 @@
                     </div>
 
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="" placeholder="From" />
-                        <x-text-input name="max_salary" value="" placeholder="To" />
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
                     </div>
                 </div>
             </div>
