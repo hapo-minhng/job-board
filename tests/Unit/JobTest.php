dd9d184272a8ee9b6de5fa993513c22b598041b0
Minh Nguyen <minhng@haposoft.com>
2024-12-23 11:54:18 +0700

    Test filter method: Filter jobs by salary

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -88,4 +88,75 @@ class JobTest extends TestCase
         $this->assertCount(2, $filteredJobsByTitle);
         $this->assertCount(1, $filteredJobsByDesc);
     }
+
+    public function test_filter_jobs_by_salary()
+    {
+        $employers = Employer::factory(3)->create();
+        $jobList = [
+            [
+                "title" => "Job A",
+                "description" => "Job A desc",
+                "salary" => 2000,
+                "location" => "NY",
+                "category" => "IT",
+                "experience" => "intermediate",
+                "employer_id" => $employers->random()->id,
+            ],
+
+            [
+                "title" => "Job B",
+                "description" => "Job B desc",
+                "salary" => 1000,
+                "location" => "Rome",
+                "category" => "Marketing",
+                "experience" => "entry",
+                "employer_id" => $employers->random()->id,
+            ],
+
+            [
+                "title" => "Job C",
+                "description" => "Job C desc",
+                "salary" => 3000,
+                "location" => "Beijing",
+                "category" => "Sales",
+                "experience" => "senior",
+                "employer_id" => $employers->random()->id,
+            ],
+
+            [
+                "title" => "Job A-1",
+                "description" => "Job A-1 desc",
+                "salary" => 5000,
+                "location" => "NY",
+                "category" => "IT",
+                "experience" => "senior",
+                "employer_id" => $employers->random()->id,
+            ]
+        ];
+
+        foreach ($jobList as $job) {
+            Job::factory()->create($job);
+        }
+
+        $filterByMinSalary = [
+            'min_salary' => 2000,
+        ];
+
+        $filterByMaxSalary = [
+            'max_salary' => 2000,
+        ];
+
+        $filterBySalary = [
+            'min_salary' => 3000,
+            'max_salary' => 4000,
+        ];
+
+        $filteredJobsByMinSalary = Job::filter($filterByMinSalary)->get();
+        $filteredJobsByMaxSalary = Job::filter($filterByMaxSalary)->get();
+        $filteredJobsBySalary = Job::filter($filterBySalary)->get();
+
+        $this->assertCount(3, $filteredJobsByMinSalary);
+        $this->assertCount(2, $filteredJobsByMaxSalary);
+        $this->assertCount(1, $filteredJobsBySalary);
+    }
 }
