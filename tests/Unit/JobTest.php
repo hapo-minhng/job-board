1aee3582613e8892079c7f62feaaf3635db2ba39
Minh Nguyen <minhng@haposoft.com>
2024-12-23 13:02:15 +0700

    Test filter method: Filter jobs by experience

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -159,4 +159,62 @@ class JobTest extends TestCase
         $this->assertCount(2, $filteredJobsByMaxSalary);
         $this->assertCount(1, $filteredJobsBySalary);
     }
+
+    public function test_filter_jobs_by_experience()
+    {
+        $employers = Employer::factory(3)->create();
+        $jobList = [
+            [
+                "title" => "Job A",
+                "description" => "Job A desc",
+                "salary" => 2000,
+                "location" => "NY",
+                "category" => "IT",
+                "experience" => "intermediate",
+                "employer_id" => $employers->random()->id,
+            ],
+
+            [
+                "title" => "Job B",
+                "description" => "Job B desc",
+                "salary" => 1000,
+                "location" => "Rome",
+                "category" => "Marketing",
+                "experience" => "entry",
+                "employer_id" => $employers->random()->id,
+            ],
+
+            [
+                "title" => "Job C",
+                "description" => "Job C desc",
+                "salary" => 3000,
+                "location" => "Beijing",
+                "category" => "Sales",
+                "experience" => "senior",
+                "employer_id" => $employers->random()->id,
+            ],
+
+            [
+                "title" => "Job A-1",
+                "description" => "Job A-1 desc",
+                "salary" => 5000,
+                "location" => "NY",
+                "category" => "IT",
+                "experience" => "senior",
+                "employer_id" => $employers->random()->id,
+            ]
+        ];
+
+        foreach ($jobList as $job) {
+            Job::factory()->create($job);
+        }
+
+        $filterByExperience = [
+            'experience' => 'senior',
+        ];
+
+        $filteredJobsByExperience = Job::filter($filterByExperience)->get();
+
+        $this->assertCount(2, $filteredJobsByExperience);
+    }
 }
