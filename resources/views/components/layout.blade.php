b26a29bde6bbf2962d74ce1aba716e9559365f56
Minh Nguyen <minhng@haposoft.com>
2024-12-24 11:43:49 +0700

    Update flash message

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -43,7 +43,7 @@ bg-gradient-to-r from-indigo-200 from-10% via-sky-200 via-30% to-emerald-200 to-
     @if (session('success'))
         <div x-data="{ flash: true }">
             <div x-show="flash">
-                <div class="mb-4 relative rounded border border-green-500 text-green-700 bg-green-100 px-4 py-3"
+                <div class="my-8 relative rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75 px-4 py-3"
                     role="alert">
 
                     <div><strong class="font-bold">Success!</strong></div>
@@ -63,7 +63,7 @@ bg-gradient-to-r from-indigo-200 from-10% via-sky-200 via-30% to-emerald-200 to-
     @if (session('error'))
         <div x-data="{ flash: true }">
             <div x-show="flash">
-                <div class="mb-4 relative rounded border border-red-500 text-red-700 bg-red-100 px-4 py-3"
+                <div class="my-8 relative rounded-md border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75 px-4 py-3"
                     role="alert">
 
                     <div><strong class="font-bold">Error!</strong></div>
