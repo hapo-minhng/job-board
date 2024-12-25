1adb89598af7fcc9397986182f3dd8460b031041
Minh Nguyen <minhng@haposoft.com>
2024-12-25 15:27:21 +0700

    Add pagination to my job application and my job

--- app/Http/Controllers/MyJobApplicationController.php
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -22,7 +22,7 @@ class MyJobApplicationController extends Controller
                             ->withTrashed(),
                         'job.employer'
                     ])
-                    ->latest()->get()
+                    ->latest()->paginate(10)
             ]
         );
     }
--- app/Http/Controllers/MyJobController.php
+++ app/Http/Controllers/MyJobController.php
@@ -19,8 +19,7 @@ class MyJobController extends Controller
                 'jobs' => auth()->user()->employer
                     ->jobs()
                     ->with(['employer', 'jobApplication', 'jobApplication.user'])
-                    ->withTrashed()
-                    ->get()
+                    ->paginate(10)
             ]
         );
     }
