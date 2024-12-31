a51b78c3052e21c4c989cb72942a5947222690e1
Minh Nguyen <minhng@haposoft.com>
2024-12-31 16:07:15 +0700

    commit change

--- tests/Feature/ManageUserTest.php
+++ tests/Feature/ManageUserTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Models\User;
 
 class ManageUserTest extends TestCase
 {
