d1ad3f903cddd5da41ce00a4934de445c0b18593
Minh Nguyen <minhng@haposoft.com>
2024-12-31 13:41:01 +0700

    Test user can manage user info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -11,7 +12,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+        return view('user.index', $user);
     }
 
     /**
