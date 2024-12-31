6849b836077500e89e3b9aa1ecc645a3a3e93e82
Minh Nguyen <minhng@haposoft.com>
2024-12-31 11:38:02 +0700

    Add manage user test

---
+++ tests/Feature/ManageUserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ManageUserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_guest_cannot_manage_user_info() {
+        $response = $this->get(route("user.edit"));
+
+        $response->assertRedirect(route("login"));
+    }
+}
